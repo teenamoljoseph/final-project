dab21091a0bbf416895f74fd4ac47c69326445f7
teenamoljoseph <37173862+teenamoljoseph@users.noreply.github.com>
2019-02-07 16:48:49 +0530

    Add files via upload

--- app-routing.module.ts
+++ app-routing.module.ts
@@ -1,7 +1,9 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { BooklistComponent } from './booklist/booklist.component';
+import { BookComponent } from './book/book.component';
 
-const routes: Routes = [];
+const routes: Routes = [{path:"booklist",component:BooklistComponent},{path:"book",component:BookComponent}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- app.module.ts
+++ app.module.ts
@@ -4,15 +4,19 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
-import { FooterComponent } from './footer/footer.component';
-import { SidebarComponent } from './sidebar/sidebar.component';
+import { BookComponent } from './book/book.component';
+import { BooklistComponent } from './booklist/booklist.component';
+import { StudentComponent } from './student/student.component';
+import { StudentlistComponent } from './studentlist/studentlist.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    FooterComponent,
-    SidebarComponent
+    BookComponent,
+    BooklistComponent,
+    StudentComponent,
+    StudentlistComponent
   ],
   imports: [
     BrowserModule,
